c9cebd7c041d8455c6384e4450f692455b53ac08
Tim <tim@cyface.com>
2024-08-06 18:03:13 -0600

    Get version from manifest.

--- index.js
+++ index.js
@@ -12,99 +12,104 @@ const octokit = github.getOctokit(actionToken)
 const owner = github.context.payload.repository.owner.login
 const repo = github.context.payload.repository.name
 
-async function updatePackage () {
-  try {
+async function updatePackage() {
+    try {
 
-    // Download release
-    const latestRelease = await octokit.rest.repos.getLatestRelease({
-      owner: owner,
-      repo: repo,
-    })
-    console.log(latestRelease.data.assets)
+        // Download release
+        const latestRelease = await octokit.rest.repos.getLatestRelease({
+            owner: owner,
+            repo: repo,
+        })
+        console.log(latestRelease.data.assets)
 
-    // Get the Asset ID of the version file from the release info
-    let assetID = 0
-    for (const item of latestRelease.data.assets) {
-      if (item.name === manifestFileName) {
-        assetID = item.id
-      }
-    }
-    if (assetID === 0) {
-      console.log(latestRelease)
-      core.setFailed('No AssetID for manifest file')
-    }
-
-    const manifestAssetUrl = `https://api.github.com/repos/${owner}/${repo}/releases/assets/${assetID}`
-    console.log(manifestAssetUrl)
-
-    const manifestAssetResponse = await fetch(manifestAssetUrl)
-    console.log(manifestAssetResponse)
-    const assetFileData = await manifestAssetResponse.json()
-    console.log(assetFileData)
-    const manifestFileResponse = await fetch(assetFileData.browser_download_url)
-    const manifestFileData = await manifestFileResponse.json()
-    console.log(manifestFileData)
-    console.log(manifestFileData.version)
-
-    const version = manifestFileData.version
-
-    const compatibilityMaxFromManifest = manifestFileData.compatibility?.maximum
-    console.log(compatibilityMaxFromManifest)
-
-    const compatibilityMinFromManifest = manifestFileData.compatibility?.minimum
-    console.log(compatibilityMinFromManifest)
-
-    const compatibilityVerifiedFromManifest = manifestFileData.compatibility?.verified
-    console.log(compatibilityVerifiedFromManifest)
-
-    const releaseNotesUrl = `https://github.com/${owner}/${repo}/releases/tag/${version}`
-    console.log(releaseNotesUrl)
-
-    const foundryResponse = await fetch("https://api.foundryvtt.com/_api/packages/release_version/", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': foundryToken
-      },
-      method: "POST",
-      body: JSON.stringify({
-        "id": repo,
-        "dry-run": dryRun,
-        "release": {
-          "version": version,
-          "manifest": assetFileData.browser_download_url,
-          "notes": releaseNotesUrl,
-          "compatibility": {
-            "minimum": compatibilityMinFromManifest,
-            "verified": compatibilityVerifiedFromManifest,
-            "maximum": compatibilityMaxFromManifest
-          }
+        // Get the Asset ID of the version file from the release info
+        let assetID = 0
+        for (const item of latestRelease.data.assets) {
+            if (item.name === manifestFileName) {
+                assetID = item.id
+            }
         }
-      })
-    })
-    console.log(foundryResponse)
-    if (foundryResponse.status === 200) {
-      const foundryResponseData = await foundryResponse.json()
-      console.log(foundryResponseData)
-    } else {
-      core.setFailed(foundryResponse.statusText)
-    }
+        if (assetID === 0) {
+            console.log(latestRelease)
+            core.setFailed('No AssetID for manifest file')
+        }
 
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        const manifestAssetUrl = `https://api.github.com/repos/${owner}/${repo}/releases/assets/${assetID}`
+        console.log(manifestAssetUrl)
+
+        const manifestAssetResponse = await fetch(manifestAssetUrl, {
+            headers: {
+                Authorization: `token ${actionToken}`, Accept: 'application/octet-stream'
+            }
+        })
+        console.log(manifestAssetResponse)
+        const assetFileData = await manifestAssetResponse.json()
+        console.log(assetFileData)
+
+        const manifestFileResponse = await fetch(assetFileData.browser_download_url)
+        const manifestFileData = await manifestFileResponse.json()
+        console.log(manifestFileData)
+        console.log(manifestFileData.version)
+
+        const version = manifestFileData.version
+
+        const compatibilityMaxFromManifest = manifestFileData.compatibility?.maximum
+        console.log(compatibilityMaxFromManifest)
+
+        const compatibilityMinFromManifest = manifestFileData.compatibility?.minimum
+        console.log(compatibilityMinFromManifest)
+
+        const compatibilityVerifiedFromManifest = manifestFileData.compatibility?.verified
+        console.log(compatibilityVerifiedFromManifest)
+
+        const releaseNotesUrl = `https://github.com/${owner}/${repo}/releases/tag/${version}`
+        console.log(releaseNotesUrl)
+
+        const foundryResponse = await fetch("https://api.foundryvtt.com/_api/packages/release_version/", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': foundryToken
+            },
+            method: "POST",
+            body: JSON.stringify({
+                "id": repo,
+                "dry-run": dryRun,
+                "release": {
+                    "version": version,
+                    "manifest": assetFileData.browser_download_url,
+                    "notes": releaseNotesUrl,
+                    "compatibility": {
+                        "minimum": compatibilityMinFromManifest,
+                        "verified": compatibilityVerifiedFromManifest,
+                        "maximum": compatibilityMaxFromManifest
+                    }
+                }
+            })
+        })
+        console.log(foundryResponse)
+        if (foundryResponse.status === 200) {
+            const foundryResponseData = await foundryResponse.json()
+            console.log(foundryResponseData)
+        } else {
+            core.setFailed(foundryResponse.statusText)
+        }
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
-async function run () {
-  try {
-    // Validate manifestFileName
-    if (manifestFileName !== 'system.json' && manifestFileName !== 'module.json')
-      core.setFailed('manifestFileName must be system.json or module.json')
+async function run() {
+    try {
+        // Validate manifestFileName
+        if (manifestFileName !== 'system.json' && manifestFileName !== 'module.json')
+            core.setFailed('manifestFileName must be system.json or module.json')
 
-    await updatePackage()
+        await updatePackage()
 
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run()
