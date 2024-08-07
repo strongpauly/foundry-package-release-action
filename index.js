b720f4ba1f80baff08f798b3a2cfad4f070cd80f
Tim <tim@cyface.com>
2024-08-06 18:42:52 -0600

    Get version from manifest.

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function updatePackage() {
         console.log(foundryToken)
 
         console.log("Dry Run")
-        const dryRunBoolean = (myValue === 'true');
+        const dryRunBoolean = (dryRun === 'true');
         console.log(dryRunBoolean)
 
         const foundryResponse = await fetch("https://api.foundryvtt.com/_api/packages/release_version/", {
